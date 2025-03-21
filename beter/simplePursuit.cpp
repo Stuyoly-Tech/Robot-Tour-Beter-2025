44097f06255911a18ed6f7ca174146d02cd2ff1b
Checed Ligh <checedligh@gmail.com>
2025-03-20 21:51:19 -0400

    slows down at end (its geeked)

--- beter/simplePursuit.cpp
+++ beter/simplePursuit.cpp
@@ -7,17 +7,15 @@ using namespace Eigen;
 #include "utils.h"
 
 float simplePursuit::getDist(Vector2f p1, Vector2f p2) {
-  return sqrt(pow((p2(0)-p1(0)), 2) + pow((p2(1)-p1(1)), 2));
+  return sqrt(pow((p2(0) - p1(0)), 2) + pow((p2(1) - p1(1)), 2));
 }
 
 //path will never be larger than 100
 void simplePursuit::init(
-  Vector2f *iPath, int iPathSize, 
-  Vector2f *iGates, int iGateSize, 
-  float iTargetTime, 
-  float iFinalOffsetY, float iFinalOffsetX
-) 
-{
+  Vector2f *iPath, int iPathSize,
+  Vector2f *iGates, int iGateSize,
+  float iTargetTime,
+  float iFinalOffsetY, float iFinalOffsetX) {
   path = iPath;
   pathSize = iPathSize;
   gates = iGates;
@@ -25,18 +23,18 @@ void simplePursuit::init(
   targetTime = iTargetTime;
   finalOffsetX = iFinalOffsetX;
   finalOffsetY = iFinalOffsetY;
-  
+
   prevPointIndex = 0;
   currentGoalPointIndex = 1;
-  
-  path[pathSize-1] = Vector2f(path[pathSize-1](0)+finalOffsetX, path[pathSize-1](1)+finalOffsetY);
+
+  path[pathSize - 1] = Vector2f(path[pathSize - 1](0) + finalOffsetX, path[pathSize - 1](1) + finalOffsetY);
 
   //Calculate pathTotalDist and avgVx
   endVx = 0;
   pathTotalDist -= DIST_TO_DOWEL;
 
-  for (int i=1; i<pathSize; i++) {
-    pathTotalDist += getDist(path[i], path[i-1]);
+  for (int i = 1; i < pathSize; i++) {
+    pathTotalDist += getDist(path[i], path[i - 1]);
   }
 }
 
@@ -53,11 +51,11 @@ void simplePursuit::nextPoint() {
 }
 
 boolean simplePursuit::atLastPoint() {
-  return (currentGoalPointIndex == pathSize-1);
+  return (currentGoalPointIndex == pathSize - 1);
 }
 
 boolean simplePursuit::isAGate() {
-  for (int i=0; i<gateSize; i++) {
+  for (int i = 0; i < gateSize; i++) {
     if (path[currentGoalPointIndex] == gates[i]) {
       return true;
     }
@@ -71,18 +69,27 @@ float simplePursuit::getCurrentGoalPointDist() {
 
 float simplePursuit::getTheta() {
   return atan2(
-    path[currentGoalPointIndex](1)-path[prevPointIndex](1), 
-    path[currentGoalPointIndex](0)-path[prevPointIndex](0)
-    );
+    path[currentGoalPointIndex](1) - path[prevPointIndex](1),
+    path[currentGoalPointIndex](0) - path[prevPointIndex](0));
 }
 
 float simplePursuit::getEndVx(float t, float d) {
   float remTime = targetTime - t;
-  int a = MAX_ACC;
-  d = mm_to_steps(d, WHEEL_RADIUS, STEPS_PER_REV);
-  endVx = (a*t - sqrt(a*a*t*t - 4*a*d)) / 2;
+  float a = steps_to_mm(MAX_ACC, WHEEL_RADIUS, STEPS_PER_REV);
+  //Serial.println(a);
+  //Serial.println(d);
+  //Serial.println(remTime);
+  if (a * a * remTime * remTime - 4 * a * d > 0) {
+    endVx = (a * remTime - sqrt(a * a * remTime * remTime - 4 * a * d)) / 2;
+  }
+  else{
+    endVx = dist / remTime;
+  }
+  //Serial.println(endVx);
   if ((endVx > MAX_VEL) || (remTime < 0)) {
     endVx = MAX_VEL;
+    //Serial.println("SET MAX VX");
   }
+  //Serial.println("SET END VX");
   return endVx;
 }
